22ca67ce23cc0ce6d6299e9a32a0f9555e1fa2b0
MATHINA PRUDHVI SANKAR <139548490+Prudhvisankar2002@users.noreply.github.com>
2025-12-30 18:29:36 +0530

    Uncomment code for list collection methods

--- collections_problems/list_collection_methodes.py
+++ collections_problems/list_collection_methodes.py
@@ -1,25 +1,25 @@
-# a=[5,9,3,12,5,7]
-# n=int(input('enter any number: '))
-# i=a.index(n)
-# count=a.count(n)
-# print(i)
-# print(count)
+a=[5,9,3,12,5,7]
+n=int(input('enter any number: '))
+i=a.index(n)
+count=a.count(n)
+print(i)
+print(count)
 
 
-# a=[5,9,3,12,5,7]
-# b=a.copy()
-# b[1]=20
-# b=sorted(b)
-# print(a)
-# print(b)
+a=[5,9,3,12,5,7]
+b=a.copy()
+b[1]=20
+b=sorted(b)
+print(a)
+print(b)
 
-# a=[]
-# ch='y'
-# while ch=='y':
-#     n=int(input('enter number: '))
-#     a.append(n)
-#     ch=input('do want add element into list(y/n): ')
-# print('your is is ',a)
+a=[]
+ch='y'
+while ch=='y':
+    n=int(input('enter number: '))
+    a.append(n)
+    ch=input('do want add element into list(y/n): ')
+print('your is is ',a)
 
 
 a=[5,9,3,12,5,7]
