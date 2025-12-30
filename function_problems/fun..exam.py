0f179d916ee8048ba2eab68b952ab18dabf7d23d
MATHINA PRUDHVI SANKAR <139548490+Prudhvisankar2002@users.noreply.github.com>
2025-12-30 18:36:46 +0530

    Uncomment functions and fix indentation errors

--- function_problems/fun..exam.py
+++ function_problems/fun..exam.py
@@ -1,73 +1,73 @@
-# def f1():
-#     print("core python")
-# f1()
-# print("bye")
+def f1():
+    print("core python")
+f1()
+print("bye")
 
-# def f2():
-#     print("hello")
-# def f1():
-#     f2()
-#     print("core python")
-# f1()
-# print('bye')
+def f2():
+    print("hello")
+def f1():
+    f2()
+    print("core python")
+f1()
+print('bye')
 
-# def f1():      #error
-#     print("core python")
-#     f2()
-#     print("venkat reddy")
-# f1()
-# print('byee')
-
-
-# def f1():
-#     print("core python")
-#     f2()
-#     print('vemkat reddy')
-# def f2():
-#     print("avdanced python")
-# f1()
-# print("byee")
+def f1():      #error
+    print("core python")
+    f2()
+    print("venkat reddy")
+f1()
+print('byee')
 
 
-# def balenq():       £error
-#        print('you have balance')
-# def withdraw():
-# balenq()
-#     print('you have withdrawn')
-# balenq()
-# print('welcome to maa bank')
-# withdraw()
+def f1():
+    print("core python")
+    f2()
+    print('vemkat reddy')
+def f2():
+    print("avdanced python")
+f1()
+print("byee")
 
 
-# def search():     £error
-#     print('sreach ur iteam')
-#     print('select ur iteam')
-#     print('see ur review')
-# defbookitem():
-#     print('add to caart')
-#     print('click buy new opition')
-# def payment():
-#     print('pay amount')
-# print('welcome to amazon ')
-# search()
-# bookiteam()
-# payment()
+def balenq():       #error
+       print('you have balance')
+def withdraw():
+balenq()
+    print('you have withdrawn')
+balenq()
+print('welcome to maa bank')
+withdraw()
 
 
-# def search():
-#     print('enetr the pincode ')
-# deforderiteam():
-#     search()
-#     print('give ur order')
-# orderiteam()
-# print("thank you")
+def search():     #error
+    print('sreach ur iteam')
+    print('select ur iteam')
+    print('see ur review')
+defbookitem():
+    print('add to caart')
+    print('click buy new opition')
+def payment():
+    print('pay amount')
+print('welcome to amazon ')
+search()
+bookiteam()
+payment()
 
-#
-# def f1 ():
-#     print('hai')
-#     f2 ()
-# def f2 ():
-#     print("hello")
-#     f1 ()
-# f2 ()
+
+def search():
+    print('enetr the pincode ')
+deforderiteam():
+    search()
+    print('give ur order')
+orderiteam()
+print("thank you")
+
+
+def f1 ():
+    print('hai')
+    f2 ()
+def f2 ():
+    print("hello")
+    f1 ()
+f2 ()
 
