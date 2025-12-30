12e2a77498d7ee4ef0f5b94afdb92e1125f888f1
MATHINA PRUDHVI SANKAR <139548490+Prudhvisankar2002@users.noreply.github.com>
2025-12-30 18:37:46 +0530

    Uncommented banking functions for deposits and withdrawals

--- function_problems/global..py
+++ function_problems/global..py
@@ -1,21 +1,21 @@
-# amount=5000
-# def  deposit():
-#      global amount
-#      damount=2000
-#      print('enter deposit amount is:',damount)
-#      amount=amount+damount
-# def  withdraw():
-#      global amount
-#      wamount=5000
-#      print('enter withdraw amount is:',wamount)
-#      amount=amount-wamount
-#      print('blance amount is:',amount)
-# def  balance():
-#     global amount
-#     print('enter balance amount is:',amount)
-# deposit()
-# withdraw()
-# balance()
+amount=5000
+def  deposit():
+     global amount
+     damount=2000
+     print('enter deposit amount is:',damount)
+     amount=amount+damount
+def  withdraw():
+     global amount
+     wamount=5000
+     print('enter withdraw amount is:',wamount)
+     amount=amount-wamount
+     print('blance amount is:',amount)
+def  balance():
+    global amount
+    print('enter balance amount is:',amount)
+deposit()
+withdraw()
+balance()
 
 
 a=20
