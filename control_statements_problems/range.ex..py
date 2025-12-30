ba1c19db6cbf80fb877a484b687c7676cf253017
MATHINA PRUDHVI SANKAR <139548490+Prudhvisankar2002@users.noreply.github.com>
2025-12-30 18:32:49 +0530

    Refactor range.ex..py to remove commented code

--- control_statements_problems/range.ex..py
+++ control_statements_problems/range.ex..py
@@ -1,64 +1,64 @@
-# n=int(input('enter any number :'))
-# for i in range(1,n+1):
-#     if i%2==0:
-#         print(i)
-# print('even numbers:',i)
-#
-#
-#
-# n=int(input('enter any number:'))
-# for i in range(n,1,-1):
-#     print(i)
-#
-#
-#
-# n=int(input('enter  any number:'))
-# sum=0
-# for i in range(1,n+1):
-#     if i%2==0:
-#         sum+=i
-# print('even numbers:',sum)
-#
-#
-#
-# n=int(input('enter the any number:'))
-# product=1
-# for i in range(1,n+1):
-#     product*=i
-# print('product is=',product)
-#
-#
-#
-# n=int(input('entered the any number:'))
-# for i in range(1,n+1):
-#     if i%5==0:
-#         print(i)
-#
-#
-#
-# n=int(input('enter the any  number :'))
-# print('odd\teven')
-# for i in range(1,n+1,):
-#     if i%2==0:
-#         print(i)
-#     else:
-#         print(i,end="\t ")
-#
-#
-#
-#
-#
-# startno=int(input('enter the any number:'))
-# endno=int(input('enter the any number:'))
-# step=0
-# for i in range(startno,endno):
-#      if startno>endno:
-#          step=-1
-#          endno=endno-1
-#      else:
-#          step=1
-#          endno=endno+1
-#      print(i,end='\t')
+n=int(input('enter any number :'))
+for i in range(1,n+1):
+    if i%2==0:
+        print(i)
+print('even numbers:',i)
+
+
+
+n=int(input('enter any number:'))
+for i in range(n,1,-1):
+    print(i)
+
+
+
+n=int(input('enter  any number:'))
+sum=0
+for i in range(1,n+1):
+    if i%2==0:
+        sum+=i
+print('even numbers:',sum)
+
+
+
+n=int(input('enter the any number:'))
+product=1
+for i in range(1,n+1):
+    product*=i
+print('product is=',product)
+
+
+
+n=int(input('entered the any number:'))
+for i in range(1,n+1):
+    if i%5==0:
+        print(i)
+
+
+
+n=int(input('enter the any  number :'))
+print('odd\teven')
+for i in range(1,n+1,):
+    if i%2==0:
+        print(i)
+    else:
+        print(i,end="\t ")
+
+
+
+
+
+startno=int(input('enter the any number:'))
+endno=int(input('enter the any number:'))
+step=0
+for i in range(startno,endno):
+     if startno>endno:
+         step=-1
+         endno=endno-1
+     else:
+         step=1
+         endno=endno+1
+     print(i,end='\t')
 
 
 
